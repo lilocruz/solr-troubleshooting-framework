d908a73d6b9faa4f0415633d8959cc14cbe9bcc3
Michael Sanchez <superlinux.michael5@gmail.com>
2023-05-15 00:11:09 -0400

    Adding the setup.py file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='solr-troubleshooting',
+    version='0.1',
+    description='A Solr troubleshooting framework',
+    url='https://github.com/yourusername/solr-troubleshooting',
+    author='Your Name',
+    author_email='your.email@example.com',
+    license='MIT',
+    packages=['solr_troubleshooting'],
+    install_requires=[
+        'requests',
+    ],
+    zip_safe=False
+)
