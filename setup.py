2a0455c7594c28e1e4e0e7c42738f747f075cfe2
Michael Sanchez <superlinux.michael5@gmail.com>
2023-05-15 00:21:16 -0400

    Fixing typo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='stsf',
+    name='solr_troubleshooting_framework',
     version='0.1',
     description='A simple but useful Apache Solr troubleshooting framework to start debuggin Apache Solr installation and settings issues.',
     url='https://github.com/lilocruz/solr-troubleshooting-framework',
