195ea0a2ae93a16618887dd78f64525919a2df79
Michael Cruz Sanchez <superlinux.michael5@gmail.com>
2023-05-16 00:25:25 -0400

    Update solr_troubleshooting.py
    Added logs check function

--- solr_troubleshooting.py
+++ solr_troubleshooting.py
@@ -1,7 +1,7 @@
 # This is the first fuction of an Apache Solr troubleshooting framework that i'm creating.
-# This fuction will perform a quick ping check.
 # Right now the script is not very mature!!.
-# AUTHOR: Michael Sanchez (Lucidworks)
+# Copytright: GPLv3
+# AUTHOR: Michael Sanchez (Search Engineer at Lucidworks)
 
 import requests
 
@@ -15,7 +15,7 @@ def check_solr_ping(solr_url):
         pass
     return False
 
-# Core check
+# Core check function
 def check_solr_core(solr_url, core_name):
     # Check if the core exists
     try:
@@ -26,7 +26,7 @@ def check_solr_core(solr_url, core_name):
         pass
     return False
 
-# Query check
+# Query check function
 def check_solr_query(solr_url, query):
     # Check if a Solr query returns any results
     try:
@@ -39,7 +39,7 @@ def check_solr_query(solr_url, query):
         pass
     return False
 
-# Field check
+# Field check function
 def check_solr_field(solr_url, core_name, field_name):
     # Check if a Solr core has a field
     try:
@@ -50,7 +50,7 @@ def check_solr_field(solr_url, core_name, field_name):
         pass
     return False
 
-# Log check
+# Log check function
 def check_solr_log(solr_log_path, error_message):
     # Check if a specific error message appears in the Solr log file
     try:
