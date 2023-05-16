6a604e93f548af563682f262069caa7a0aee91ab
lilocruz <superlinux.michael5@gmail.com>
2023-05-16 01:27:52 -0400

    init file modified

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""
+stsf.
+
+A Python module for troubleshooting Apache Solr installations.
+"""
+
+__version__ = "0.1"
+__author__ = 'Michael Sanchez'
+__credits__ = 'Michael Sanchez Search Engineer @lucidworks'
